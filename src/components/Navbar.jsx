9f7f724ef04dbb31dceb13dcaafaf102b702578f
unknown <adriancruzemail@gmail.com>
2026-05-24 14:09:31 +0800

    Removed the FontAwesome import icons temporarily and still ongoing troubleshoot

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import AdrianAvatar from "../assets/images/AdrianDevAvatar.jpg"
-import FontAwesomeIcon from "@fortawesome/react-fontawesome"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+
 
 const Navbar = () => {
     return (
         <div className="flex flex-row items-center justify-left gap-5 border bg-gray-800 border-gray-700 mx-3 rounded-2xl px-5 py-6 ">
             <div className="flex flex-row items-center justify-center gap-2">
-                <FontAwesomeIcon icon={faBars} className="text-white text-2xl cursor-pointer" />
+`               
                 <img src={AdrianAvatar} alt="Adrian Cruz" className="w-10 h-10  rounded-md" />
             </div>
             <div>
