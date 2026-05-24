ec3a74134da5d66a74b179f7afeec2586dfe6397
unknown <adriancruzemail@gmail.com>
2026-05-24 16:15:38 +0800

    Added a fullDescription data, icons for Details button and View Website hyperlink with icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
 import { ProjectsData } from '../data/ProjectsData';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faAngleDown, faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons';
 
 const Card = () => {
+
     return (
         <>
             {ProjectsData.map((project, index) => (
-                <div className="mx-5 p-5 flex flex-col gap-3 border border-gray-600 rounded-2xl">
-
+                <div className="mx-5 p-5 flex flex-col gap-3 border bg-gray-800/40 border-gray-600 rounded-2xl">
 
                     <div key={index} className="w-full h-35 overflow-hidden rounded-lg">
                         <img src={project.image} alt="Beat Jam Preview" />
@@ -16,12 +18,26 @@ const Card = () => {
                             <h1 className="text-gray-400 text-sm font-light">{project.type}</h1>
                             <h1 className="text-white font-bold">{project.title}</h1>
                         </div>
-                        <div>
-                            <button className="text-white text-xs border border-gray-600 px-6 py-2 rounded-full">DETAILS</button>
+
+                        {/* Button Details */}
+                        <div className="flex flex-col items-center justify-center">
+                            <button className="text-white text-xs border border-gray-600 px-3 py-2 rounded-full cursor-pointer">
+                                <span className="mr-1">DETAILS</span>
+                                <FontAwesomeIcon icon={faAngleDown} className="" />
+                            </button>
                         </div>
                     </div>
                     <div>
                         <p className="text-gray-400 text-sm">{project.description}</p>
+                    </div>
+
+                    <div>
+                        <p className="text-gray-400 text-sm">{project.fullDescription}</p>
+                    </div>
+                    <div className="mt-2">
+                        <a href={project.websiteLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm hover:text-blue-400">
+                            Visit Website<FontAwesomeIcon icon={faSquareArrowUpRight} className="ml-1" />
+                        </a>
                     </div>
                 </div>
             ))}
--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -7,15 +7,17 @@ export const ProjectsData = [
         type: "RESTAURANT BAR",
         title: "Beat Jam Restobar",
         description: "A modern restaurant bar with a vibrant atmosphere and delicious cocktails.",
+        fullDescription: "Beat Jam Restobar is a modern restaurant bar that offers a vibrant atmosphere and a wide selection of delicious cocktails. The website features a sleek design with high-quality images, an easy-to-navigate menu, and an online reservation system. It also includes a gallery showcasing the ambiance of the bar and customer testimonials to enhance the user experience.",
         image: BeatJamPreview,
-        websiteLink: "#"
+        websiteLink: "https://cruzadriaan.github.io/BeatJam/#/"
     },
     {
         stack: "FRONT END",
         type: "BUILDERS AND CONSTRUCTION",
         title: "Ponari Builders",
         description: "A construction company specializing in residential and commercial projects.",
+        fullDescription: "Ponari Builders is a reputable construction company that specializes in both residential and commercial projects. With a commitment to quality and customer satisfaction, they deliver exceptional results on every job. Their portfolio includes a diverse range of buildings, from modern homes to large-scale commercial developments.",
         image: PonariPreview,
-        websiteLink: "#"
+        websiteLink: "https://ponari-builders.vercel.app/"
     }
 ]
