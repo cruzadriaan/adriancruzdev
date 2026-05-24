60ac238a2057035f1766381ac61212ea5b177a3f
unknown <adriancruzemail@gmail.com>
2026-05-24 18:35:49 +0800

    Added socialmedia icons in hero section and added a glowing border effect to the avatar in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="fixed left-1/2 -translate-x-1/2 xl:w-290 lg:w-220 md:w-170 sm:w-140 w-95 flex flex-row items-center justify-left gap-3 border bg-gray-800 border-blue-600  rounded-3xl px-5 py-6 ">
             <div className="flex flex-row items-center justify-center gap-5">
                 <FontAwesomeIcon icon={faBars} className="text-gray-200 text-xl border border-gray-600 rounded-md p-2 cursor-pointer" />
-                <img src={AdrianAvatarEnhanced} alt="Adrian Cruz" className="w-10 h-10  rounded-md" />
+                <img src={AdrianAvatarEnhanced} alt="Adrian Cruz" className="w-10 h-10 border border-blue-600 rounded-md" />
             </div>
             <div>
                 <h1 className="text-white font-semibold cursor-pointer">Adrian Cruz</h1>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,18 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons';
+import {  faSquareArrowUpRight, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faSquareLinkedin, faSquareGithub} from '@fortawesome/free-brands-svg-icons';
 
 const Hero = () => {
     return (
         <div className="flex flex-col sm:w-120 w-90 gap-5 mt-28 mb-10 lg:mx-11 md:mx-8 sm:mx-8 mx-5">
-            <div className="items-left justify-center text-white font-bold lg:text-6xl text-5xl">
-                <h1>Hello, I'm</h1>
-                <h1 className="text-blue-400">Adrian Cruz</h1>
+            <div className="items-left justify-center lg:text-6xl text-5xl">
+                <h1 className="text-white text-4xl font-bold">Hello, I'm</h1>
+                <h1 className="text-blue-400 font-bold">Adrian Cruz</h1>
             </div>
             <div>
                 <h1 className="text-gray-300 lg:text-lg text-[14px] font-semibold">TURNING IDEAS INTO INTERACTIVE,<br/> HIGH PERFORMANCE WEBSITES.</h1>
             </div>
             <div>
                 <h1 className="text-white lg:text-base text-[14px] font-light">I build responsive, Full‑stack web applications that deliver exceptional digital experiences. My focus is on blending modern design with robust functionality, turning ideas into seamless, high‑performance solutions.</h1>
+            </div>
+            <div className="flex flex-row gap-4">
+                <a href="https://www.linkedin.com/in/adrian-cruzdev/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faSquareLinkedin} className="text-gray-200 text-2xl rounded-full hover:scale-110 cursor-pointer" />
+                </a>
+                <a href="https://github.com/cruzadriaan" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faSquareGithub} className="text-gray-200 text-2xl rounded-full hover:scale-110 cursor-pointer" />
+                </a>
+                <a href="mailto:adriancruzemail@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faEnvelope} className="text-gray-200 text-2xl rounded-full hover:scale-110 cursor-pointer" />
+                </a>
             </div>
             <div className="flex flex-row gap-4 mt-2 text-sm">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold lg:py-3 lg:px-5 py-2 px-4 rounded-full cursor-pointer">
