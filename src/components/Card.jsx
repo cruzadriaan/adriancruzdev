3bf245c77f2dcdd1d9bcc55b8c2820008b89bca9
unknown <adriancruzemail@gmail.com>
2026-05-24 16:25:24 +0800

    Adjusted image size capture to fit the mobile view preview

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,8 +9,8 @@ const Card = () => {
             {ProjectsData.map((project, index) => (
                 <div className="mx-5 p-5 flex flex-col gap-3 border bg-gray-800/40 border-gray-600 rounded-2xl">
 
-                    <div key={index} className="w-full h-35 overflow-hidden rounded-lg">
-                        <img src={project.image} alt="Beat Jam Preview" />
+                    <div key={index} className="w-full h-45  rounded-lg">
+                        <img src={project.image} alt="Beat Jam Preview" className="h-full w-full object-fit" />
                     </div>
                     <div className="flex flex-row justify-between  ">
                         <div>
@@ -28,11 +28,11 @@ const Card = () => {
                         </div>
                     </div>
                     <div>
-                        <p className="text-gray-400 text-sm">{project.description}</p>
+                        <p className="text-gray-400 text-xs">{project.description}</p>
                     </div>
 
                     <div>
-                        <p className="text-gray-400 text-sm">{project.fullDescription}</p>
+                        <p className="text-gray-400 text-xs">{project.fullDescription}</p>
                     </div>
                     <div className="mt-2">
                         <a href={project.websiteLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 text-sm hover:text-blue-400">
