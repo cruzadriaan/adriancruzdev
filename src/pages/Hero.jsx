15f805c0d158ce455d80a7d6c23daab47107029e
unknown <adriancruzemail@gmail.com>
2026-05-25 09:36:56 +0800

    Resized the ExploreMyWork and ViewMyResume button in hero section for more minimalist environment

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -28,8 +28,8 @@ const Hero = () => {
             </div>
 
             {/* BUTTONS */}
-            <div className="flex flex-row items-center flex-wrap w-100 gap-4 mt-2 text-sm">
-                <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold lg:py-3 lg:px-5 w-45 py-2 px-6 rounded-full cursor-pointer">
+            <div className="flex flex-row items-center flex-wrap w-110 gap-4 mt-2 text-sm">
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold lg:py-3 lg:px-5 w-43 py-2 px-3 rounded-full cursor-pointer">
                     <FontAwesomeIcon icon={faSquareArrowUpRight} className="mr-1" />
                     Explore My Work
                 </button>
@@ -37,7 +37,7 @@ const Hero = () => {
                     <FontAwesomeIcon icon={faSquareArrowUpRight} className="mr-1" />
                     Get in Touch
                 </button>
-                <a href="/files/CruzAdrianCV.pdf" target="_blank" rel="noopener noreferrer" className="border border-white hover:bg-gray-800 text-white font-semibold lg:py-3 lg:px-5 w-45 py-2 px-6 rounded-full cursor-pointer">
+                <a href="/files/CruzAdrianCV.pdf" target="_blank" rel="noopener noreferrer" className="border border-white hover:bg-gray-800 text-white text-center font-semibold lg:py-3 lg:px-5 w-43 py-2 px-3 rounded-full cursor-pointer">
                     <FontAwesomeIcon icon={faFileLines} className="mr-1" />
                     View my Resume
                 </a>
