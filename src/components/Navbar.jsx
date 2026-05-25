f4051f16d6547b62877c5af9b8ee2b1a2a683ee6
unknown <adriancruzemail@gmail.com>
2026-05-25 10:55:21 +0800

    Added a Menu file to be rendered in sidebar, added sidebar in Navbar and make the toggleButton working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,62 @@
 // import AdrianAvatar from "../assets/images/AdrianDevAvatar.jpg";
 import AdrianAvatarEnhanced from "../assets/images/AdrianDevAvatarEnhanced.png";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBars} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Menu } from "../data/Menu";
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
     return (
-        <div className="fixed left-1/2 -translate-x-1/2 xl:w-290 lg:w-220 md:w-170 sm:w-140 w-95 flex flex-row items-center justify-left gap-3 border bg-gray-800 border-blue-600  rounded-3xl px-5 py-6 ">
-            <div className="flex flex-row items-center justify-center gap-5">
-                <FontAwesomeIcon icon={faBars} className="text-gray-200 text-xl border bg-gray-700 shadow-md border-gray-600 rounded-md p-2 cursor-pointer" />
-                <img src={AdrianAvatarEnhanced} alt="Adrian Cruz" className="w-10 h-10 border border-blue-800 rounded-md" />
+        <>
+            <div className="fixed left-1/2 -translate-x-1/2 xl:w-290 lg:w-220 md:w-170 sm:w-140 w-95 flex flex-row items-center justify-left gap-3 border bg-gray-800 border-blue-600  rounded-3xl px-5 py-6 ">
+                <div className="flex flex-row items-center justify-center gap-5">
+                    <button onClick={() => setIsSidebarOpen(true)}>
+                        <FontAwesomeIcon icon={faBars} className="text-gray-200 text-xl border bg-gray-700 shadow-md border-gray-600 rounded-md p-2 cursor-pointer" />
+                    </button>
+                    <img src={AdrianAvatarEnhanced} alt="Adrian Cruz" className="w-10 h-10 border border-blue-800 rounded-md" />
+                </div>
+                <div>
+                    <h1 className="text-white font-semibold cursor-pointer">Adrian Cruz</h1>
+                    <h1 className="text-gray-400 text-sm font-semibold">Front End Developer</h1>
+                </div>
             </div>
-            <div>
-                <h1 className="text-white font-semibold cursor-pointer">Adrian Cruz</h1>
-                <h1 className="text-gray-400 text-sm font-semibold">Front End Developer</h1>
+
+            
+            {/* Sidebar */}
+            {isSidebarOpen && ( 
+            <div className="fixed left-0 top-0 h-full w-60 bg-gray-900 shadow-lg py-4 ">
+                <div className="flex flex-row ml-3 gap-4">
+                    <img src={AdrianAvatarEnhanced} alt="Adrian Cruz" className="w-13 h-13 border border-blue-900 mb-5 rounded-full" />
+                    <div className="flex flex-col mt-1">
+                        <h1 className="text-white text-xs font-semibold">Adrian Cruz</h1>
+                        <h2 className="text-gray-400 text-[11px]">Front End Developer</h2>
+                    </div>
+                    <div className="ml-auto mr-2">
+                        <button className="text-gray-200 hover:text-white/80 cursor-pointer"
+                            onClick={() => setIsSidebarOpen(false)}>
+                            <FontAwesomeIcon icon={faXmark} className="text-base" />
+                        </button>
+                    </div>
+                </div>
+
+                {/* Navigation Links */}
+                <div className="ml-3">
+                    <h1 className="text-gray-400 text-xs">Navigation</h1>
+                    <ul className="flex flex-col gap-3 mt-2">
+                        {Menu.map((item, index) => (
+                            <li key={index} className="text-gray-300 hover:text-blue-400 text-sm font-semibold  cursor-pointer hover:bg-gray-800 p-2 rounded-md mr-3">
+                                <FontAwesomeIcon icon={item.icon} className="mr-2" />
+                                <span>{item.name}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
+            )}
+        </>
     )
 }
 
---
+++ src/data/Menu.js
@@ -0,0 +1,24 @@
+import { faHouse, faUser, faBriefcase, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+export const Menu = [
+    {
+        name: "Home",
+        link: "#home",
+        icon: faHouse
+    },
+    {
+        name: "About",
+        link: "#about",
+        icon: faUser
+    },
+    {
+        name: "Projects",
+        link: "#projects",
+        icon: faBriefcase
+    },
+    {
+        name: "Contact",
+        link: "#contact",
+        icon: faEnvelope
+    }
+]
