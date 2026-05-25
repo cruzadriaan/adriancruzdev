737b00d196ebfe43c879e3f90f499acfca0ea365
unknown <adriancruzemail@gmail.com>
2026-05-25 18:26:50 +0800

    Reduced spacing between dot and sectionName divider

--- src/components/SectionDivider.jsx
+++ src/components/SectionDivider.jsx
@@ -4,7 +4,7 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 const SectionDivider = (props) => {
     return (
         <>
-            <div className="flex items-center text-white text-xs font-normal mx-5 my-3 border border-gray-600 w-fit px-4 py-1 rounded-full">
+            <div className="flex items-center text-white text-xs font-normal mx-5 my-3 border border-gray-600 w-fit px-3 py-1 rounded-full">
                 <FontAwesomeIcon icon={faCircle} className="text-green-500 text-[5px] mr-2" />
                 <span>{props.section}</span>
             </div>
