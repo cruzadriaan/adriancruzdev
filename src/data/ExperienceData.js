0c27fd08a1a9e1ae6fa8ed7c76097974f8a65cc6
unknown <adriancruzemail@gmail.com>
2026-05-26 21:29:06 +0800

    Added workExperience description using map and added a button for toggleFullDetails option

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -3,10 +3,22 @@ export const ExperienceData = [
         company: "WESERV SYSTEMS INC.",
         position: "Application Systems Engineer",
         duration: "April 2022 - Present",
+        achieve:[
+            "Executed a 2‑year modernization migration project using SDLC (Waterfall).",
+            "Managed multi-platform databases (MySQL, SQL Server).",
+            "Built responsive web apps with React + Tailwind CSS.",
+            "Streamlined workflows with Git, Azure DevOps, and ServiceNow.",
+            "Practiced Agile/Scrum with sprints for better collaboration."
+        ]
     },
     {
         company: "CONCENTRIX",
         position: "Technical Support Representative",
         duration: "May 2021 - January 2022",
+        achieve:[
+            "Provided hosting and domain support, including purchases and renewals.",
+            "Maintained server security and performance.",
+            "Advised clients on hosting solutions (shared, VPS, dedicated, WordPress)."
+        ]
     },
 ]
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,8 +2,12 @@ import SectionDivider from "../components/SectionDivider";
 import { ExperienceData } from "../data/ExperienceData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
 
 const Experience = ({ containerClass }) => {
+
+    const [isDetailsOpen, setIsDetailsOpen] = useState(null);
+
     return (
         <div className="mb-10">
             <SectionDivider section="EXPERIENCE" />
@@ -13,17 +17,28 @@ const Experience = ({ containerClass }) => {
                         <div className="flex flex-col mb-2">
                             <h1 className="text-white font-bold">{experience.company}</h1>
                             <h1 className="text-blue-400  font-semibold">{experience.position}</h1>
+                            <p className="text-gray-400 text-[12px] mt-1">{experience.duration}</p>
                         </div>
                         <div>
-                            <button className="flex items-center justify-center w-7 h-7 border hover:bg-gray-800 border-gray-600 shadow-2xs shadow-blue-700 cursor-pointer rounded-full">
+                            <button className="flex items-center justify-center w-7 h-7 border hover:bg-gray-800 border-gray-600 shadow-2xs shadow-blue-700 cursor-pointer rounded-full"
+                            onClick={() => setIsDetailsOpen(isDetailsOpen === null ? null : index)}>
                                 <FontAwesomeIcon icon={faAngleDown} className="text-sm text-white/80"></FontAwesomeIcon>
                             </button>
                         </div>
                     </div>
-                    <div className="border-t border-gray-700 " />
-                    <div>
-                        <p className="text-gray-400 text-[12px]">{experience.duration}</p>
+
+                    <div className="border-t border-gray-700" />
+
+                    {isDetailsOpen === index &&
+                    <div className="text-white text-sm pl-5 pt-3">
+                        <ul className="list-disc text-[13px] text-gray-300 flex flex-col gap-1">
+                            {experience.achieve.map((exp, i) => (
+                                <li key={i}>{exp}</li>
+                            ))
+                            }
+                        </ul>
                     </div>
+                    }
                 </div>
             ))}
         </div>
