513879a9a6be8a9cb06e6edbee8b98707ab07dd6
unknown <adriancruzemail@gmail.com>
2026-05-26 13:20:20 +0800

    Adjusted border color in the Experience and Techstack section and reduced size of icons in techStack

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,14 +6,16 @@ const Experience = ({ containerClass }) => {
         <div className="mb-10">
             <SectionDivider section="EXPERIENCE" />
             {ExperienceData.map((experience, index) => (
-            <div key={index} className={`flex flex-col gap-2 mb-4 ${containerClass}`}>
-                <h1 className="text-white font-bold">{experience.company}</h1>
-                <h1 className="text-blue-400  font-semibold">{experience.position}</h1>
-                <div className="border-t border-gray-500 " />
-                <div>
-                    <p className="text-gray-400 text-sm">{experience.duration}</p>
+                <div key={index} className={`flex flex-col mb-4 ${containerClass}`}>
+                    <div className="flex flex-col mb-2">
+                        <h1 className="text-white font-bold">{experience.company}</h1>
+                        <h1 className="text-blue-400  font-semibold">{experience.position}</h1>
+                    </div>
+                    <div className="border-t border-gray-700 " />
+                    <div>
+                        <p className="text-gray-400 text-[12px]">{experience.duration}</p>
+                    </div>
                 </div>
-            </div>
             ))}
         </div>
     )
--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -13,18 +13,18 @@ const TechStack = ({ containerClass }) => {
 
           {/* Tech Category */}
           <div>
-            <div className={`flex items-center gap-2 mb-2 ${tech.categoryColor}`}>
-              <FontAwesomeIcon icon={tech.logo} className="text-2xl" />
-              <h1 className="text-xl font-semibold">{tech.category}</h1>
+            <div className={`flex items-center gap-1 mb-1 ${tech.categoryColor}`}>
+              <FontAwesomeIcon icon={tech.logo} className="text-xl" />
+              <h1 className="text-lg font-semibold">{tech.category}</h1>
             </div>
-            <div className="border-t border-gray-600" />
+            <div className="border-t border-gray-700" />
 
             {/* Tech Stack Each */}
             <div className="flex items-center justify-start flex-wrap gap-7 mt-3">
               {tech.technologies.map((technology, index) => (
                 <div key={index} className="flex flex-col items-center gap-1 hover:scale-110 transition-transform cursor-pointer">
                   <i className={`${technology.logo} colored text-3xl`} ></i>
-                  <h1 className="text-gray-200 text-[12px] font-semibold">{technology.name}</h1>
+                  <h1 className="text-gray-200 text-[11px] font-semibold">{technology.name}</h1>
                 </div>
               ))}
             </div>
