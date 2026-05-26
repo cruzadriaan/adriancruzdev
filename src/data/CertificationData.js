bc38e327132fc77731b95d4e0e397fe93d68b385
unknown <adriancruzemail@gmail.com>
2026-05-26 10:04:28 +0800

    Reduced margin of certificationCard

---
+++ src/data/CertificationData.js
@@ -0,0 +1,17 @@
+import AWSCCP from "../assets/icons/AWS_CCP.png";
+import MTA from "../assets/icons/MTA.png";
+
+export const CertificationData = [
+    {
+        title: "AWS Certified Cloud Practitioner",
+        issued: "January 2026",
+        image: AWSCCP,
+        imageSize: "w-18 h-20"
+    },
+    {
+        title: "Microsoft Technology Associate in HTML and CSS",
+        issued: "November 2019",
+        image: MTA,
+        imageSize: "w-18 h-18"
+    }
+]
--- src/pages/Certification.jsx
+++ src/pages/Certification.jsx
@@ -1,26 +1,22 @@
 import SectionDivider from '../components/SectionDivider';
-import AWSCCP from "../assets/icons/AWS_CCP.png";
-import MTA from "../assets/icons/MTA.png";
+import { CertificationData } from '../data/CertificationData';
 
 const Certification = () => {
     return (
         <div className="mb-10">
             <SectionDivider section="CERTIFICATION" />
-            <div className="m-5 py-5 flex flex-col gap-2 border bg-gray-800/40 border-gray-600 rounded-2xl">
-                <div className=" mx-3 px-5 py-4 flex flex-row items-center  gap-4 mb-2 border bg-gray-800/40 border-gray-600 rounded-2xl">
-                    <img src={AWSCCP} alt="AWS Certified Cloud Practitioner" className="w-18 h-20" />
-                    <div className="">
-                        <h1 className="text-sm font-bold text-blue-400">AWS Certified Cloud Practitioner</h1>
-                        <p className="text-gray-400 text-xs">Issued: January 2026</p>
+            <div className="mx-5 py-5 flex flex-col gap-2 border bg-gray-800/40 border-gray-600 rounded-2xl">
+                {CertificationData.map((certification, index) => (
+                    <div key={index}>
+                        <div className=" mx-3 px-5 py-4 flex flex-row items-center  gap-4 mb-2 border bg-gray-800/40 border-gray-600 rounded-2xl">
+                            <img src={certification.image} alt={certification.title} className={certification.imageSize} />
+                            <div className="">
+                                <h1 className="text-sm font-bold text-blue-400">{certification.title}</h1>
+                                <p className="text-gray-400 text-xs">Issued: {certification.issued}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="mx-3 px-5 py-4 flex flex-row items-center gap-4 border bg-gray-800/40 border-gray-600 rounded-2xl">
-                    <img src={MTA} alt="Microsoft Technology Associate" className="w-18 h-18" />
-                    <div className="">
-                        <h1 className="text-sm font-bold text-blue-400">Microsoft Technology Associate in HTML and CSS</h1>
-                        <p className="text-gray-400 text-xs">Issued: November 2019</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
