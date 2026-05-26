29c84666fb7935da20dec85210db0c7777062ebf
unknown <adriancruzemail@gmail.com>
2026-05-26 09:54:55 +0800

    Adjusted certification badge image size to match other badges alignment

--- src/pages/Certification.jsx
+++ src/pages/Certification.jsx
@@ -6,17 +6,16 @@ const Certification = () => {
     return (
         <div className="mb-10">
             <SectionDivider section="CERTIFICATION" />
-            <div className="m-5 p-3 flex flex-col gap-2 border bg-gray-800/40 border-gray-600 rounded-2xl">
-                <div className=" mx-5 p-5 flex flex-row items-center justify-center gap-3 mb-3 border bg-gray-800/40 border-gray-600 rounded-2xl">
-
-                    <img src={AWSCCP} alt="AWS Certified Cloud Practitioner" className="w-25 h-25 mx-auto" />
+            <div className="m-5 py-5 flex flex-col gap-2 border bg-gray-800/40 border-gray-600 rounded-2xl">
+                <div className=" mx-3 px-5 py-4 flex flex-row items-center  gap-4 mb-2 border bg-gray-800/40 border-gray-600 rounded-2xl">
+                    <img src={AWSCCP} alt="AWS Certified Cloud Practitioner" className="w-18 h-20" />
                     <div className="">
                         <h1 className="text-sm font-bold text-blue-400">AWS Certified Cloud Practitioner</h1>
                         <p className="text-gray-400 text-xs">Issued: January 2026</p>
                     </div>
                 </div>
-                <div className="mx-5 p-5 flex flex-row items-center justify-center gap-3 mb-3 border bg-gray-800/40 border-gray-600 rounded-2xl">
-                    <img src={MTA} alt="Microsoft Technology Associate" className="w-26 h-26 mx-auto" />
+                <div className="mx-3 px-5 py-4 flex flex-row items-center gap-4 border bg-gray-800/40 border-gray-600 rounded-2xl">
+                    <img src={MTA} alt="Microsoft Technology Associate" className="w-18 h-18" />
                     <div className="">
                         <h1 className="text-sm font-bold text-blue-400">Microsoft Technology Associate in HTML and CSS</h1>
                         <p className="text-gray-400 text-xs">Issued: November 2019</p>
