56f7bcce1dde582d724b4236ffa7e02ac1ce7624
unknown <adriancruzemail@gmail.com>
2026-05-27 09:49:08 +0800

    Made the Connect form each details a hyperlink

--- src/sections/contact/ContactMe.jsx
+++ src/sections/contact/ContactMe.jsx
@@ -22,14 +22,14 @@ const ContactMe = ({ containerClass }) => {
             <div className={`flex flex-col gap-2 mb-5 ${containerClass}`}>
                 <h1 className="text-[11px] text-gray-500 ml-1">PERSONAL</h1>
                 {ContactDetails.personal.map((personal, index) => (
-                    <div key={index} className="flex flex-row items-center gap-1 text-gray-300 hover:scale-105 w-75 px-1 py-2 rounded-lg cursor-pointer">
-                        <FontAwesomeIcon icon={personal.icon} className="mr-2 text-lg bg-linear-to-r from-blue-800 to-purple-800 p-3 rounded-lg" />
-                        <div className='text-sm'>
-                            <h1 className='font-bold'>{personal.type}</h1>
-                            <a href={personal.link} target="_blank" rel="noopener noreferrer" className=''>
-                                {personal.title}
-                            </a>
-                        </div>
+                    <div key={index} className="text-gray-300 hover:scale-105 w-75 px-1 py-2 rounded-lg cursor-pointer">
+                        <a href={personal.link} target="_blank" rel="noopener noreferrer" className="flex flex-row items-center gap-1">
+                            <FontAwesomeIcon icon={personal.icon} className="mr-2 text-lg bg-linear-to-r from-blue-800 to-purple-800 p-3 rounded-lg" />
+                            <div className='text-sm'>
+                                <h1 className='font-bold'>{personal.type}</h1>
+                                <a>{personal.title}</a>
+                            </div>
+                        </a>
                     </div>
                 ))}
 
@@ -38,14 +38,16 @@ const ContactMe = ({ containerClass }) => {
                     <h1 className="text-[11px] text-gray-500 mt-3 ml-1">SOCIAL</h1>
                     <div className='flex flex-row gap-4'>
                         {ContactDetails.social.map((social, index) => (
-                            <div key={index} className="flex flex-row items-center gap-1 text-gray-300 hover:scale-105 px-1 py-2 rounded-lg cursor-pointer">
+                            <div key={index} className="text-gray-300 hover:scale-105 px-1 py-2 rounded-lg cursor-pointer">
+                                <a href={social.link} target="_blank" rel="noopener noreferrer" className="flex flex-row items-center gap-1">
                                 <FontAwesomeIcon icon={social.icon} className="mr-2 text-lg bg-linear-to-r from-blue-800 to-purple-800 p-3 rounded-lg" />
                                 <div className="text-sm">
                                     <h1 className='font-bold'>{social.type}</h1>
-                                    <a href={social.link} target="_blank" rel="noopener noreferrer">
+                                    <a>
                                         {social.title}
                                     </a>
                                 </div>
+                                </a>
                             </div>
                         ))}
                     </div>
