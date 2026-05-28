04dbee0fff62ef68068d5805ee6510bf5d24509b
unknown <adriancruzemail@gmail.com>
2026-05-28 14:40:51 +0800

    Adjusted the paragraph padding and added temporary animation scroll (for mobile checking purposes)

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ import { faEnvelope, faFileLines, faExternalLinkAlt } from '@fortawesome/free-so
 import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
 import { faSquareLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 import { motion } from 'framer-motion';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const Hero = ({ hardTextGradient, mediumTextGradient }) => {
 
@@ -16,6 +16,7 @@ const Hero = ({ hardTextGradient, mediumTextGradient }) => {
         <div
             className="relative flex flex-col sm:w-120 w-90 gap-5 mt-35 lg:mx-11 md:mx-8 sm:mx-8 mx-5 mb-25
         ">
+            {/* Name introduction */}
             <motion.div
                 variants={cardVariants}
                 initial="hidden"
@@ -25,38 +26,44 @@ const Hero = ({ hardTextGradient, mediumTextGradient }) => {
                 <h1 className="text-white text-3xl font-bold mb-1">Hello, I'm</h1>
                 <h1 className={`${hardTextGradient} text-5xl md:text-7xl font-extrabold`}>Adrian Cruz</h1>
             </motion.div>
+
+            {/* Tagline */}
             <motion.div
                 variants={cardVariants}
                 initial="hidden"
                 animate="visible"
                 transition={{ duration: 1 }}>
-                <h1 className="text-gray-300 lg:text-lg text-[14px] font-semibold">TURNING IDEAS INTO INTERACTIVE,<br /> HIGH PERFORMANCE WEBSITES.</h1>
+                <h1 className="mt-1 text-gray-300 lg:text-lg text-[14px] font-semibold">TURNING IDEAS INTO INTERACTIVE,<br /> HIGH PERFORMANCE WEBSITES.</h1>
             </motion.div>
+
+            {/* Paragraph */}
             <motion.div
                 initial={{ y: 30, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 1 }}>
-                <h1 className="mt-2 text-gray-300 text-base w-70 leading-relaxed"><span className={mediumTextGradient}>I build modern, responsive web interfaces</span> that deliver exceptional digital experiences — blending design expertise with <span className={mediumTextGradient}>Frontend development</span>, and growing toward <span className={mediumTextGradient}>Full‑stack</span>.</h1>
+                <h1 className="mt-4 text-gray-300 text-base w-70"><span className={mediumTextGradient}>I build modern, responsive web interfaces</span> that deliver exceptional digital experiences — blending design expertise with <span className={mediumTextGradient}>Frontend development</span>, and growing toward <span className={mediumTextGradient}>Full‑stack</span>.</h1>
             </motion.div>
+
+            {/* Social Icons */}
             <motion.div
                 initial={{ y: 30, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 1 }}
-            className="flex flex-row gap-4">
+                className="mt-1 flex flex-row gap-4">
                 <a href="https://www.linkedin.com/in/adrian-cruzdev/" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faSquareLinkedin} className="text-gray-200 text-2xl rounded-full hover:scale-110 cursor-pointer" />
+                    <FontAwesomeIcon icon={faSquareLinkedin} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
                 </a>
                 <a href="https://github.com/cruzadriaan" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faSquareGithub} className="text-gray-200 text-2xl rounded-full hover:scale-110 cursor-pointer" />
+                    <FontAwesomeIcon icon={faSquareGithub} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
                 </a>
                 <a href="mailto:adriancruzemail@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faEnvelope} className="text-gray-200 text-2xl rounded-full hover:scale-110 cursor-pointer" />
+                    <FontAwesomeIcon icon={faEnvelope} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
                 </a>
             </motion.div>
 
             {/* BUTTONS */}
-            <div className="flex flex-row  items-center flex-wrap w-85 gap-4 mt-1 text-sm">
-                <Link to='project' smooth={true} duration={800} offset={-100} className="bg-linear-to-r hover:from-blue-600 hover:to-purple-600 from-blue-500 to-purple-500 text-white text-center font-semibold lg:py-3 lg:px-5 w-43 py-2 px-3 rounded-xl cursor-pointer">
+            <div className="mt-1 flex flex-row items-center flex-wrap w-85 gap-4 text-sm">
+                <Link to='project' smooth={true} duration={800} offset={-100} className="bg-linear-to-r hover:from-blue-700 hover:to-purple-700 from-blue-600 to-purple-600 text-white text-center font-semibold lg:py-3 lg:px-5 w-43 py-2 px-3 rounded-xl cursor-pointer">
                     <FontAwesomeIcon icon={faExternalLinkAlt} className="mr-1" />
                     Explore My Work
                 </Link>
@@ -68,6 +75,19 @@ const Hero = ({ hardTextGradient, mediumTextGradient }) => {
                     <FontAwesomeIcon icon={faFileLines} className="mr-1" />
                     View my Resume
                 </a>
+            </div>
+
+            {/* Scroll Indicator */}
+            <div className="relative mt-20" >
+                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 mb-5">
+                    <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex items-start justify-center p-1">
+                        <motion.div
+                            animate={{ y: [0, 10, 0] }}
+                            transition={{ duration: 1.5, repeat: Infinity }}
+                            className="w-2 h-2 bg-gray-400 rounded-full"
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
