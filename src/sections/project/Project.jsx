ea829db05f9e0d9ce5426e4ce6f57306c88c940d
unknown <adriancruzemail@gmail.com>
2026-05-28 17:40:07 +0800

    Switched the framer motion to only SectionDivider instead of whole container to be more optimized and sync with Project card

--- src/sections/project/Project.jsx
+++ src/sections/project/Project.jsx
@@ -5,19 +5,23 @@ import { cardVariants } from "../../utils/cardVariants";
 
 const Projects = ({ containerClass, mediumTextGradient, softTextGradient }) => {
     return (
-        <motion.div
-            variants={cardVariants}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.1 }}
-            transition={{ duration: 1.5 }}
-        >
-            <SectionDivider section="PROJECTS" />
+        <div>
+
+            <motion.div
+                variants={cardVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.1 }}
+                transition={{ duration: 0.8 }}
+            >
+                <SectionDivider section="PROJECTS" />
+            </motion.div>
 
             <div className="flex flex-col gap-5 mb-12 md:grid md:grid-cols-2 lg:mx-5 md:mx-2 sm:mx-12">
                 <ProjectCard containerClass={containerClass} mediumTextGradient={mediumTextGradient} softTextGradient={softTextGradient} />
             </div>
-        </motion.div>
+            
+        </div >
     )
 }
 
