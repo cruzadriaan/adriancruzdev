e2e88224d992e1bd39654f2438caa857db5ff32f
unknown <adriancruzemail@gmail.com>
2026-05-28 19:10:11 +0800

    Removed the stats in About me section since I already added in Hero section

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -1,7 +1,7 @@
 import SectionDivider from "../components/SectionDivider";
 import AdrianDevAvatar from "../assets/images/AdrianDevAvatarEnhanced.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { AboutMeData } from "../data/AboutMeData";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { AboutMeData } from "../data/AboutMeData";
 import { motion } from 'framer-motion';
 import { cardVariants } from "../utils/cardVariants";
 
@@ -36,14 +36,16 @@ const AboutMe = ({ containerClass, mediumTextGradient }) => {
                         I’m a passionate Front-End Developer dedicated to crafting engaging, intuitive, and visually striking web experiences. With a solid foundation in HTML, CSS, and JavaScript, I specialize in building responsive interfaces that balance functionality and design. I love transforming complex ideas into seamless user journeys that feel effortless and natural.
                     </p>
                 </div>
-                <div className="flex flex-row items-center gap-3 justify-between mt-5">
+
+                {/* Personal Stats */}
+                {/* <div className="flex flex-row items-center gap-3 justify-between mt-5">
                     {AboutMeData.map((item, index) => (
                         <div key={index} className="flex flex-col items-center gap-0.5 my-auto border border-gray-600 rounded-lg bg-gray-900 p-2.5 h-32 w-30 cursor-pointer text-center">
                             <FontAwesomeIcon icon={item.icon} className="text-green-600 shadow-2xl bg-blue-900/50 rounded-full p-2 text-2xl " />
                             <h1 className="text-white text-[13px] font-semibold w-20">{item.title}</h1>
                         </div>
                     ))}
-                </div>
+                </div> */}
 
             </div>
         </motion.div>
