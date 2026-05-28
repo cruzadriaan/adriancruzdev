02f795acd632b8a637a351e48e01268775b8cb30
unknown <adriancruzemail@gmail.com>
2026-05-28 21:21:42 +0800

    Added image background in hero section. This one is tricky part because of z-index layering to show up the svg background imaage, relative z-0 fixed this

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faFileLines, faExternalLinkAlt, faAnglesDown } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faFileLines, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
 import { faSquareLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 import { motion } from 'framer-motion';
@@ -14,84 +14,93 @@ const Hero = ({ hardTextGradient, mediumTextGradient }) => {
     }
 
     return (
-        <div
-            className="mt-28 relative flex flex-col sm:w-120 w-90 gap-5 lg:mx-11 md:mx-8 sm:mx-8 mx-5 mb-25
-        ">
-            {/* Name introduction */}
-            <motion.div
-                variants={heroVariants}
-                initial="hidden"
-                animate="visible"
-                transition={{ duration: 1 }}
-                className="items-left justify-center lg:text-6xl">
-                <h1 className="text-white text-3xl font-bold mb-1">Hello, I'm</h1>
-                <h1 className={`${hardTextGradient} text-5xl md:text-7xl font-extrabold`}>Adrian Cruz</h1>
-            </motion.div>
+        <div className="relative h-230 mb-12">
 
-            {/* Tagline */}
-            <motion.div
-                variants={heroVariants}
-                initial="hidden"
-                animate="visible"
-                transition={{ duration: 1 }}>
-                <h1 className="mt-1 text-gray-300 lg:text-lg text-[14px] font-semibold">TURNING IDEAS INTO INTERACTIVE,<br /> HIGH PERFORMANCE WEBSITES.</h1>
-            </motion.div>
+            {/* Background Base */}
+            <div className="absolute inset-0 bg-black -z-30" />
 
-            {/* Paragraph */}
-            <motion.div
-                initial={{ y: 30, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 1 }}>
-                <h1 className="mt-3 text-gray-300 text-base w-70"><span className={mediumTextGradient}>I build modern, responsive web interfaces</span> that deliver exceptional digital experiences — blending design expertise with <span className={mediumTextGradient}>Frontend development</span>, and growing toward <span className={mediumTextGradient}>Full‑stack</span>.</h1>
-            </motion.div>
+            {/* Background Image */}
+            <div className="absolute inset-0 bg-center opacity-10 -z-20  bg-[url('assets/waveEffect.svg')]" />
 
-            {/* Social Icons */}
-            <motion.div
-                initial={{ y: 30, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 1 }}
-                className="mt-1 flex flex-row gap-4">
-                <a href="https://www.linkedin.com/in/adrian-cruzdev/" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faSquareLinkedin} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
-                </a>
-                <a href="https://github.com/cruzadriaan" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faSquareGithub} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
-                </a>
-                <a href="mailto:adriancruzemail@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faEnvelope} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
-                </a>
-            </motion.div>
+            {/* Content */}
+            <div className="relative pt-28 flex flex-col sm:w-120 w-full gap-5 lg:mx-11 md:mx-8 sm:mx-8 px-5 mb-25 z-10">
 
-            {/* BUTTONS */}
-            <div className="mt-1 flex flex-row items-center flex-wrap w-85 gap-4 text-sm">
-                <Link to='project' smooth={true} duration={800} offset={-100} className="bg-linear-to-r hover:from-blue-700 hover:to-purple-700 from-blue-600 to-purple-600 text-white text-center font-semibold lg:py-3 lg:px-5 w-43 py-2 px-3 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faExternalLinkAlt} className="mr-1" />
-                    Explore My Work
-                </Link>
-                <Link to="contact" smooth={true} duration={800} offset={-100} className="bg-white hover:bg-white/90 text-black font-semibold lg:py-3 lg:px-5 py-2 px-5 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faCommentDots} className="mr-1 font-semibold" />
-                    Get in Touch
-                </Link>
-                <a href="/files/CruzAdrianCV.pdf" target="_blank" rel="noopener noreferrer" className="border border-gray-400 hover:bg-white/5 text-white text-center font-semibold lg:py-3 lg:px-5 w-82 py-3 mt-1 px-3 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faFileLines} className="mr-1" />
-                    View my Resume
-                </a>
-            </div>
+                {/* Name introduction */}
+                <motion.div
+                    variants={heroVariants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 1 }}
+                    className="items-left justify-center lg:text-6xl">
+                    <h1 className="text-white text-3xl font-bold mb-1">Hello, I'm</h1>
+                    <h1 className={`${hardTextGradient} text-5xl md:text-7xl font-extrabold`}>Adrian Cruz</h1>
+                </motion.div>
 
-            {/* Hero Stats */}
-            <div className="mt-10 grid grid-cols-2 md:grid-cols-4 gap-4">
-                {HeroData.map((stat, index) => (
-                    <motion.div
-                        key={index}
-                        initial={{ y: 30, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        transition={{ duration: 1, delay: index * 0.2 }}
-                        className="bg-gray-800/40 backdrop-blur-md py-6 px-4 rounded-xl border border-gray-700"
-                    >
-                        <h2 className={`${mediumTextGradient} text-2xl font-bold`}>{stat.count}</h2>
-                        <p className="text-gray-400 text-[13px]">{stat.title}</p>
-                    </motion.div>
-                ))}
+                {/* Tagline */}
+                <motion.div
+                    variants={heroVariants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ duration: 1 }}>
+                    <h1 className="mt-1 text-gray-300 lg:text-lg text-[14px] font-semibold">TURNING IDEAS INTO INTERACTIVE,<br /> HIGH PERFORMANCE WEBSITES.</h1>
+                </motion.div>
+
+                {/* Paragraph */}
+                <motion.div
+                    initial={{ y: 30, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}>
+                    <h1 className="mt-3 text-gray-300 text-base w-70"><span className={mediumTextGradient}>I build modern, responsive web <span className={mediumTextGradient}>interfaces</span></span> that deliver exceptional digital experiences — blending design expertise with <span className={mediumTextGradient}>Frontend development</span>, and growing toward <span className={mediumTextGradient}>Full‑stack</span>.</h1>
+                </motion.div>
+
+                {/* Social Icons */}
+                <motion.div
+                    initial={{ y: 30, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    className="mt-1 flex flex-row gap-4">
+                    <a href="https://www.linkedin.com/in/adrian-cruzdev/" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faSquareLinkedin} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
+                    </a>
+                    <a href="https://github.com/cruzadriaan" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faSquareGithub} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
+                    </a>
+                    <a href="mailto:adriancruzemail@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faEnvelope} className="text-gray-200 text-3xl rounded-full hover:scale-110 cursor-pointer" />
+                    </a>
+                </motion.div>
+
+                {/* BUTTONS */}
+                <div className="mt-1 flex flex-row items-center flex-wrap w-85 gap-4 text-sm">
+                    <Link to='project' smooth={true} duration={800} offset={-100} className="bg-linear-to-r hover:from-blue-700 hover:to-purple-700 from-blue-600 to-purple-600 text-white text-center font-semibold lg:py-3 lg:px-5 w-43 py-2 px-3 rounded-xl cursor-pointer">
+                        <FontAwesomeIcon icon={faExternalLinkAlt} className="mr-1" />
+                        Explore My Work
+                    </Link>
+                    <Link to="contact" smooth={true} duration={800} offset={-100} className="bg-white hover:bg-white/90 text-black font-semibold lg:py-3 lg:px-5 py-2 px-5 rounded-xl cursor-pointer">
+                        <FontAwesomeIcon icon={faCommentDots} className="mr-1 font-semibold" />
+                        Get in Touch
+                    </Link>
+                    <a href="/files/CruzAdrianCV.pdf" target="_blank" rel="noopener noreferrer" className="border border-gray-400 hover:bg-white/5 text-white text-center font-semibold lg:py-3 lg:px-5 w-82 py-3 mt-1 px-3 rounded-xl cursor-pointer">
+                        <FontAwesomeIcon icon={faFileLines} className="mr-1" />
+                        View my Resume
+                    </a>
+                </div>
+
+                {/* Hero Stats */}
+                <div className="mt-8 w-90 grid grid-cols-2 md:grid-cols-4 gap-4">
+                    {HeroData.map((stat, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ y: 30, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 1, delay: index * 0.2 }}
+                            className="bg-gray-800/40 backdrop-blur-md py-6 px-4 rounded-xl border border-gray-700"
+                        >
+                            <h2 className={`${mediumTextGradient} text-2xl font-bold`}>{stat.count}</h2>
+                            <p className="text-gray-400 text-[13px]">{stat.title}</p>
+                        </motion.div>
+                    ))}
+                </div>
             </div>
 
             {/* Scroll Indicator */}
