965418e856925b790ac6483fa15b70627cb7ec22
unknown <adriancruzemail@gmail.com>
2026-05-30 16:32:20 +0800

    Added animate bounce in scroll up arrow

--- src/components/FloatingIcons.jsx
+++ src/components/FloatingIcons.jsx
@@ -2,14 +2,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faViber } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-scroll';
+import { useRef, useEffect } from 'react';
+
 
 const FloatingIcons = () => {
+
     return (
         <>
             {/* Floating Arrow Up */}
             <div className="fixed bottom-15 right-0 z-20" >
                 <Link to="hero" smooth={true} duration={500}>
-                    <FontAwesomeIcon icon={faArrowUp} className="text-gray-200 bg-gray-800 text-base border border-gray-600 rounded-full p-2.5 m-2 hover:scale-110 cursor-pointer" />
+                    <FontAwesomeIcon icon={faArrowUp} className="text-gray-200 bg-gray-800 text-base border border-gray-600 rounded-full p-2.5 m-2 hover:scale-110 cursor-pointer animate-bounce" />
                 </Link>
             </div>
 
