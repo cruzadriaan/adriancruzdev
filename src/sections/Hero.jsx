57075aa151c9b687d27e3c91e2112e1f38361988
unknown <adriancruzemail@gmail.com>
2026-06-02 10:00:32 +0800

    Changed the min-h-screen of hero to min-h-fit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = ({ hardTextGradient }) => {
 
         <div
             ref={heroRef}
-            className="relative min-h-screen overflow-hidden">
+            className="relative min-h-fit overflow-hidden">
 
             <FloatingButtons showArrowTop={showArrowTop} />
 
