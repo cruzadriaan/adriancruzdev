a9ce04366a6f0495bb7b75d8b3b277ddf0125dd1
unknown <adriancruzemail@gmail.com>
2026-06-02 09:37:43 +0800

    Reduced padding bottom of Hero section for proper space for About section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = ({ hardTextGradient }) => {
 
         <div
             ref={heroRef}
-            className="relative mb-16 min-h-screen overflow-hidden">
+            className="relative mb-10 min-h-screen overflow-hidden">
 
             <FloatingButtons showArrowTop={showArrowTop} />
 
