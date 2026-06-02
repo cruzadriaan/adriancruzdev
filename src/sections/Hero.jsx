7b0b5653bfb22a8198a18d835cb32925f546d4cd
unknown <adriancruzemail@gmail.com>
2026-06-02 09:45:28 +0800

    Combined the dvh and min-h-screen for mobile and web compatibility (trial)

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = ({ hardTextGradient }) => {
 
         <div
             ref={heroRef}
-            className="relative mb-10 min-h-screen overflow-hidden">
+            className="relative mb-10 h-[100dvh] min-h-screen overflow-hidden">
 
             <FloatingButtons showArrowTop={showArrowTop} />
 
